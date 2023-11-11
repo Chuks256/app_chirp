5a3386db2b433fc50e5916a03915d4d844660a4f
chuks <alexanderchuks310@gmail.com>
2023-11-11 09:56:50 -0800

    frustatratde

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,13 +1,13 @@
 import "../styles/footerStyle.css"
 import hand from "../assets/hand.png"
-import chirpFooter from "../assets/chirpFooter.png"
+import chirpfooter from "../assets/chirpfooter.png"
 
 let FooterComponent=()=>{
     return(
         <>
         <div className="footerContainer">
         <img src={hand} alt="" className="handImg"/>
-        <img src={chirpFooter}  className="chirpFooter" alt="" />
+        <img src={chirpfooter}  className="chirpFooter" alt="" />
         </div>
    </> 
     )
